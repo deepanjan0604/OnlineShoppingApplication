bdf822dfa75bde5dc5d0f96be9fdaded7571a058
Deepanjan <deep060494@gmail.com>
2016-02-17 14:40:32 +0530

    merged

--- OnlineShoppingApplication/src/main/java/com/dh/webtest/OnlineShoppingApplication.java
+++ OnlineShoppingApplication/src/main/java/com/dh/webtest/OnlineShoppingApplication.java
@@ -8,6 +8,5 @@ public class OnlineShoppingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShoppingApplication.class, args);
-		System.out.println("hiu");
 	}
 }
